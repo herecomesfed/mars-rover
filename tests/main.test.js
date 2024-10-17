e7fa8623ef64be58477fccfdbcd56b1189ef48d2
herecomesfed <federicograziani97@gmail.com>
2024-10-17 16:46:09 +0200

    remove global rover variable

--- tests/main.test.js
+++ tests/main.test.js
@@ -3,53 +3,53 @@ import { MarsRover } from "../classes/MarsRover";
 import { Mars } from "../classes/Mars";
 import { Obstacle } from "../classes/Obstacle";
 
-let rover;
 const createNewMarsRover = (direction) => {
   const grid = new Mars(5, 4);
   const obstacle1 = new Obstacle(3, 3);
   const obstacle2 = new Obstacle(0, 1);
-  rover = new MarsRover(0, 0, direction, grid, [obstacle1, obstacle2]);
+  const rover = new MarsRover(0, 0, direction, grid, [obstacle1, obstacle2]);
+  return rover;
 };
 
 describe("Mars rover movements without grid constraint", () => {
   test("move rover in north direction", () => {
-    createNewMarsRover("N");
+    const rover = createNewMarsRover("N");
     rover._moveRover();
     expect(rover.getPosition().y).toEqual(1);
   });
 
   test("move rover in south direction", () => {
-    createNewMarsRover("S");
+    const rover = createNewMarsRover("S");
     rover._moveRover();
     expect(rover.getPosition().y).toEqual(-1);
   });
 
   test("move rover in east direction", () => {
-    createNewMarsRover("E");
+    const rover = createNewMarsRover("E");
     rover._moveRover();
     expect(rover.getPosition().x).toEqual(1);
   });
 
   test("move rover in west direction", () => {
-    createNewMarsRover("W");
+    const rover = createNewMarsRover("W");
     rover._moveRover();
     expect(rover.getPosition().x).toEqual(-1);
   });
 
   test("rotate rover left", () => {
-    createNewMarsRover("N");
+    const rover = createNewMarsRover("N");
     rover._changeDirection("L");
     expect(rover.direction).toBe("W");
   });
 
   test("rotate rover right", () => {
-    createNewMarsRover("W");
+    const rover = createNewMarsRover("W");
     rover._changeDirection("R");
     expect(rover.direction).toBe("N");
   });
 
   test("simulate one forward movement", () => {
-    createNewMarsRover("N");
+    const rover = createNewMarsRover("N");
     rover._moveRover("F");
     expect(rover.getFullPosition()).toEqual({
       x: 0,
@@ -59,7 +59,7 @@ describe("Mars rover movements without grid constraint", () => {
   });
 
   test("simulate one backward movement", () => {
-    createNewMarsRover("N");
+    const rover = createNewMarsRover("N");
     rover._moveRover("B");
     expect(rover.getFullPosition()).toEqual({
       x: 0,
@@ -69,7 +69,7 @@ describe("Mars rover movements without grid constraint", () => {
   });
 
   test("simulate multi commands", () => {
-    createNewMarsRover("N");
+    const rover = createNewMarsRover("N");
     const commands = ["R", "F", "F", "L", "F", "B"];
     commands.forEach((command) => {
       if (command === "L" || command === "R") {
@@ -89,7 +89,7 @@ describe("Mars rover movements without grid constraint", () => {
 
 describe("Simulate movements with grid", () => {
   test("simulate touch limit on x", () => {
-    createNewMarsRover("E");
+    const rover = createNewMarsRover("E");
     const commands = ["F", "F", "F", "F", "F"];
     commands.forEach((command) => {
       rover._moveRover(command);
@@ -99,7 +99,7 @@ describe("Simulate movements with grid", () => {
   });
 
   test("simulate negative limit on x", () => {
-    createNewMarsRover("W");
+    const rover = createNewMarsRover("W");
     rover._moveRover("F");
     rover._checkGridLimits();
 
@@ -107,7 +107,7 @@ describe("Simulate movements with grid", () => {
   });
 
   test("simulate touch y limit", () => {
-    createNewMarsRover("N");
+    const rover = createNewMarsRover("N");
     const commands = ["F", "F", "F", "F"];
     commands.forEach((command) => {
       rover._moveRover(command);
@@ -117,7 +117,7 @@ describe("Simulate movements with grid", () => {
   });
 
   test("simulate negative y limit", () => {
-    createNewMarsRover("S");
+    const rover = createNewMarsRover("S");
     rover._moveRover("S");
     rover._checkGridLimits();
     expect(rover.getFullPosition().y).toEqual(3);
@@ -126,7 +126,7 @@ describe("Simulate movements with grid", () => {
 
 describe("simulate rover impact with an obstacle", () => {
   test("Simulate obstacle collision on (0,1)", () => {
-    createNewMarsRover("N");
+    const rover = createNewMarsRover("N");
     rover._isThereObstacle("F");
     expect(rover._isThereObstacle()).toEqual(true);
   });
